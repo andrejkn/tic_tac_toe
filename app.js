b67db277d06d7e17469f1903bb04e663bc9ad031
Andrej Knezevski <knezevskia@gmail.com>
2020-05-07 02:15:00 -0400

    simple tic-tac-toe

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
-const checkIfWinning = ([f1, f2, f3]) => (f1 && f1 === f2 && f1 === f3 && f2 === f3);
-
-const findWinningFields = (fields, index) => {
-    //    +0 +1 +2
-    //   +--------
-    // 0 | 0, 1, 2
-    // 3 | 3, 4, 5
-    // 6 | 6, 7, 8
-
-    const verticalPos = index % 3;
-    const vertical = [verticalPos, verticalPos + 3, verticalPos + 6];
-
-    const horizontalPos = Number.parseInt(index / 3) * 3;
-    const horizontal = [horizontalPos, horizontalPos + 1, horizontalPos + 2]
-
-    const diagonal1 = [0, 4, 8].includes(index) ? [0, 4, 8] : null;
-    const diagonal2 = [2, 4, 6].includes(index) ? [2, 4, 6] : null;
-
-    if (checkIfWinning(vertical.map((i) => fields[i]))) {
-        return vertical;
-    } else if (checkIfWinning(horizontal.map((i) => fields[i]))) {
-        return horizontal;
-    } else if (diagonal1 && checkIfWinning(diagonal1.map((i) => fields[i]))) {
-        return diagonal1;
-    } else if (diagonal2 && checkIfWinning(diagonal2.map((i) => fields[i]))) {
-        return diagonal2;
-    }
-
-    return null;
-}
-
 const grid = (() => {
     const numFields = 9;
     const players = [
@@ -46,15 +15,10 @@ const grid = (() => {
 
     return {
         markField: (index) => {
-            if (state.fields[index] === null && state.winningFields === null) {
+            if (state.fields[index] === null) {
                 state.fields[index] = players[state.turnCount % players.length];
-                state.winningFields = findWinningFields(state.fields, index);
                 state.turnCount++;
                 render();
-
-                if (state.turnCount % 2 === 1) {
-                    zombieMove();
-                }
             }
         },
         getField: (index) => state.fields[index],
@@ -65,16 +29,6 @@ const grid = (() => {
         }
     };
 })();
-
-const zombieMove = () => {
-    const emptyFields = grid.getFields()
-        .map((field, index) => field === null ? index : null)
-        .filter((freeIndex) => freeIndex !== null);
-
-    const nextIndex = Math.floor(Math.random() * emptyFields.length);
-
-    grid.markField(emptyFields[nextIndex]);
-};
 
 const Component = (tag, children, attributes, eventHandlers) => {
     const el = document.createElement(tag);
@@ -130,7 +84,6 @@ const Grid = (fields) => Component(
     {class: 'grid-container'}
 );
 
-
 const App = (children) => {
     const el = document.getElementById('app');
     el.innerHTML = null;
@@ -153,17 +106,13 @@ const render = () => {
 
     const panel = Panel([restartButton]);
 
-    const winnerFields = grid.getWinningFields();
-    const messageContainer = Message(
-        winnerFields
-            ? `${grid.getField(winnerFields[0])} is the winner!` : ''
-        );
+    const messageContainer = Message('??? is the winner!');
     const fields = grid.getFields()
         .map((field, i) => Field(
             field,
             () => grid.markField(i),
-            winnerFields ? winnerFields.includes(i) : false
-        ))
+            false
+        ));
     const gridContainer = Grid(fields);
 
     App([
